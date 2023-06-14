155b8904538f89c14c592b054fc47862cdebd1b1
carlosm22700 <carlosmartinez.steven@gmail.com>
2023-06-13 20:25:21 -0700

    deletes README

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
 console.log('js:loaded')
-const inventors = [
-    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-    { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-    { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-    { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-    { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-    { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-    { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-    { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
-  ];
+
 // Array.prototype.filter()
 // 1. Filter the array of inventors into a new array containing only the inventors born in the 1500's
 
+const inventors = [
+  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
+  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
+  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
+  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
+  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
+  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
+  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
+  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
+];
     //creates a constant oldInventors.
     //then takes the array inventors and filters out any inventor who was born after 1500
     //it then checks the second conditional. If the birth year is below 1600
@@ -26,12 +27,16 @@ console.log(oldInventors);
 // Array.prototype.map()
 // 2. Map the array of the inventors into a new array containing objects with just the first and last names as properties
 // Hint:  Return a new object literal from the callback (don't mutate the object being passed in to map)
+
 const firstAndLast = inventors.map(investor => `${investor.first} ${investor.last}`);
+
 console.log(firstAndLast)
 
 // Array.prototype.sort()
 // 3. Sort the inventors by birth date (year property), in ascending order
+
 const sortByDate = inventors.sort((a,b) => a.year - b.year);
+
 console.log(sortByDate);
 
 
@@ -39,6 +44,7 @@ console.log(sortByDate);
 // 4. Find the inventor object with the first name of 'Ada'
 
 const firstNameAda = inventors.find(inventor => inventor.first === 'Ada');
+
 console.log(firstNameAda);
 
 
@@ -121,6 +127,7 @@ console.log(isEveryoneOldEnough); // should return false
 
 // Array.prototype.find()
 // 10. Find the comment with the id of 823423
+
 const comments = [
   { text: 'Love this!', id: 523423 },
   { text: 'Super good', id: 823423 },
@@ -134,6 +141,7 @@ console.log(findCommentByID);
 
 // Array.prototype.findIndex()
 // 11. Find the index of the comment with an id of 123523
+
 const findIndexByID = comments.findIndex(comment => comment.id === 123523);
 
 console.log(findIndexByID);
